f41d13bd6e337f8d79dc9a50a5eb43f9eb30a168
= <jord52@gmail.com>
2013-06-03 14:58:35 -0400

    initial commit 	new file:   .classpath 	new file:   .gitignore 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   .settings/org.eclipse.m2e.core.prefs 	new file:   deps/ojdbc6.jar 	new file:   pom.xml 	new file:   src/main/java/ca/etsmtl/log660/configuration/SessionFactoryHelper.java 	new file:   src/main/resources/ca/etsmtl/log660/configuration/hibernate.cfg.xml 	new file:   src/main/resources/hibernate.properties 	new file:   src/main/resources/hibernate.reveng.xml 	new file:   target/classes/ca/etsmtl/log660/configuration/hibernate.cfg.xml 	new file:   target/classes/hibernate.properties 	new file:   target/classes/hibernate.reveng.xml 	new file:   target/lab01-1.0-SNAPSHOT.jar 	new file:   target/maven-archiver/pom.properties

---
+++ src/main/java/ca/etsmtl/log660/configuration/SessionFactoryHelper.java
@@ -0,0 +1,28 @@
+package ca.etsmtl.log660.configuration;
+
+import java.io.File;
+import java.net.URL;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryHelper {
+
+	private static final SessionFactory sessionFactory;
+
+	static {
+		try {
+			URL hibernateConfig = SessionFactoryHelper.class.getResource("hibernate.cfg.xml");
+			sessionFactory = new Configuration().configure(hibernateConfig)
+					.buildSessionFactory();
+		} catch (Throwable e) {
+			System.err.println("Could not create the session factory.");
+			throw new ExceptionInInitializerError(e);
+		}
+
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
